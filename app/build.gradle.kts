dc4519904cebb1b447578141ee7ebf463f206d0c
Benjamin <benj.verdejo@duocuc.cl>
2025-11-18 16:22:01 -0300

    implementacion de AuthReparaFacilRepositoryTest (todavia necesita arreglar errores)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,7 +40,26 @@ android {
 }
 
 dependencies {
+implementation(libs.firebase.appdistribution.gradle)
+    // Testing - JUnit
+    testImplementation("junit:junit:4.13.2")
 
+    // Testing - Coroutines
+    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.3")
+
+    // Testing - MockK (mocking framework)
+    testImplementation(libs.mockk)
+    androidTestImplementation(libs.mockk.android)
+
+    // Testing - Turbine (para testar StateFlow/Flow)
+    testImplementation("app.cash.turbine:turbine:1.0.0")
+
+    // Testing - Core Testing (para InstantTaskExecutorRule)
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
+
+    // Android Testing
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -79,6 +98,7 @@ dependencies {
     implementation("androidx.compose.material:material-icons-core")
     implementation("androidx.compose.material:material-icons-extended")
     implementation("com.google.accompanist:accompanist-permissions:0.36.0")
+
 
 
 
